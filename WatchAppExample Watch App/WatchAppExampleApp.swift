2debcf3c01156a162dcab41207d708660c5ec850
Caroline <carolinesalibc@gmail.com>
2024-03-21 08:50:12 -0600

    Initial Commit

---
+++ WatchAppExample Watch App/WatchAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppExampleApp.swift
+//  WatchAppExample Watch App
+//
+//  Created by Caroline Canto on 2024-03-21.
+//
+
+import SwiftUI
+
+@main
+struct WatchAppExample_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
