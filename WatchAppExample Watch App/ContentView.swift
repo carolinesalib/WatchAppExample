2e5e436c9487c2dc7c3a5804437d7c44fc83985a
Caroline <carolinesalibc@gmail.com>
2024-03-23 20:04:42 -0600

    Simple note counting functionality

--- WatchAppExample Watch App/ContentView.swift
+++ WatchAppExample Watch App/ContentView.swift
@@ -8,16 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    @State private var notes: [Note] = [Note]()
+    @State private var text: String = ""
+    
+    // MARK: - FUNCTION
+    
+    func save() {
+        dump(notes)
+    }
+    
+    // MARK: - BODY
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                TextField("Add New Note", text: $text)
+                
+                Button {
+                    guard text.isEmpty == false else { return }
+                    let note = Note(id: UUID(), text: text)
+                    
+                    notes.append(note)
+                    
+                    text = ""
+                    
+                    save()
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .font(.system(size: 45, weight: .semibold))
+                }
+                .fixedSize()
+                .buttonStyle(PlainButtonStyle())
+                .foregroundColor(.accentColor)
+            })
+            
+            Spacer()
+            
+            Text("\(notes.count)")
         }
-        .padding()
+        .navigationTitle("Notes")
     }
 }
+
+// MARK: - PREVIEW
 
 #Preview {
     ContentView()
---
+++ WatchAppExample Watch App/Model/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  WatchAppExample Watch App
+//
+//  Created by Caroline Canto on 2024-03-22.
+//
+
+import Foundation
+
+struct Note: Identifiable, Codable {
+    let id: UUID
+    let text: String
+    
+}
